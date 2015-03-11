ccd03714f89cf6ca1f34e759a0012d81133202ec
Roman <GeX@GeXs-MacBook-Air.local>
2015-03-11 19:48:59 +0300

    alpha

--- catchGoal/addAndEditGoalCell.m
+++ catchGoal/addAndEditGoalCell.m
@@ -13,12 +13,13 @@
 - (void)awakeFromNib {
     
     self.backgroundColor = [UIColor colorWithRed:0.97 green:0.97 blue:0.97 alpha:1];
-
+    self.backgroundColor = [UIColor clearColor];
     for (UIView* view in self.contentView.subviews) {
         
         if ([view isKindOfClass:[UITextField class]]) {
             UITextField* field = (UITextField*)view;
             
+            field.alpha = 0.5;
             field.layer.cornerRadius = 5.f;
             field.layer.borderColor = [UIColor colorWithRed:0.87 green:0.87 blue:0.87 alpha:1].CGColor;
             field.layer.borderWidth = 1.f;
