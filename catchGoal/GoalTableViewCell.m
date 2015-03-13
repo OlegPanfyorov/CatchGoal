0293fe1d9686dd2f4da341599d151647ddd7a579
Roman <GeX@GeXs-MacBook-Air.local>
2015-03-13 17:27:27 +0300

    sss

--- catchGoal/GoalTableViewCell.m
+++ catchGoal/GoalTableViewCell.m
@@ -13,10 +13,12 @@
 
 
 - (void)awakeFromNib {
-    self.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.17];
+    self.containerView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.17];
+    self.backgroundColor = [UIColor clearColor];
     self.lineProgressView.progressType = ProgressLabelRect;
     self.lineProgressView.backBorderWidth = 0.0;
     self.lineProgressView.frontBorderWidth = 0.0;
+    self.lineProgressView.layer.cornerRadius = self.lineProgressView.frame.size.height / 2;
     self.lineProgressView.clipsToBounds = YES;
     self.image.backgroundColor = [UIColor clearColor];
     self.image.layer.cornerRadius = self.image.frame.size.height / 2;
